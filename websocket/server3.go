def18d17772ec67a2c7d04395f9778fbd2aebfc4
Neil <ebox381@gmail.com>
2026-06-29 01:23:20 -0400

    finished chap1

---
+++ websocket/server3.go
@@ -0,0 +1,24 @@
+// handler echoes the HTTP request 
+package main
+
+import(
+	"fmt"
+	"net/http"
+	"log"
+)
+
+func handler(w http.ResponseWriter, r *http.request) {
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.Url, r.Proto)
+	for k, v := range r.Header {
+	fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
+}
+
